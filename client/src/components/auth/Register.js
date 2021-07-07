85e6088fa48c47805a4149584823a51e0fd1debb
Maria <lu_vinas@hotmail.com>
2021-07-07 14:54:47 -0400

    user registration complete, saving user to db. registration error handling working as well

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,21 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
     const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
 
     const { setAlert } = alertContext;
+
+    const { register, error, clearErrors } = authContext;
+
+    useEffect(()=> {
+        if(error === 'User already exists') {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+    }, [error]);
 
     const [user, setUser] = useState({
         name: '',
@@ -24,7 +35,11 @@ const Register = () => {
         } else if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('Register submit')
+            register({
+                name,
+                email,
+                password
+            })
         }
     };
     
@@ -42,7 +57,7 @@ const Register = () => {
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" value={password} onChange={onChange} required/>
+                    <input type="password" name="password" value={password} onChange={onChange} required min-length="6"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password2">Confirm Password</label>
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -24,14 +25,40 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState);
 
     // Load User (checking which user is logged in)
+    const loadUser = () => console.log('loaduser');
 
     // Register User (sign user up, get token back)
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users', formData, config)
+            // if OK
+            dispatch({
+                type: REGISTER_SUCCESS, 
+                payload: res.data
+            });
+            // if ERROR
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL, 
+                payload: err.response.data.msg
+            });
+        }
+    }
 
     // Login User (log user in, get token)
+    const login = () => console.log('login');
 
     // Logout (destroy token)
+    const logout = () => console.log('logout');
 
     // Clear Errors (in state)
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
     return (
         <AuthContext.Provider
@@ -41,9 +68,13 @@ const AuthState = props => {
                 loading: state.loading,
                 user: state.user,
                 error: state.error,
-
-
-            }}>
+                register,
+                loadUser,
+                login,
+                logout,
+                clearErrors
+            }}
+            >
             { props.children }
         </AuthContext.Provider>
     )
